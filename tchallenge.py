4c82ff5123e81407893065283d67769d311c8722
Clivince Estinvil <clivince.estinvil@esih.edu>
2023-11-04 19:46:03 -0400

    premye challenge

--- tchallenge.py
+++ tchallenge.py
@@ -1,1 +1,15 @@
-print('hello')
+
+chenn = "Ayibobo Ayiti"
+chenn_an_miniskil = chenn.lower()
+print(chenn_an_miniskil)
+
+
+def koupe_ekzanp(chenn):
+    eleman  = chenn.split()
+    return eleman 
+
+# Egzanp d'apre sa ou te bay:
+chenn = "Ayibobo Ayiti"
+lis = koupe_ekzanp(chenn)
+print(lis)
+
