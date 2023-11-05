321d44f0d5b4b89b4c5f61d870d27e94c5bb5bcd
Clivince Estinvil <clivince.estinvil@esih.edu>
2023-11-05 15:06:22 -0500

    version final

--- tchallenge.py
+++ tchallenge.py
@@ -12,4 +12,55 @@ def koupe_ekzanp(chenn):
 chenn = "Ayibobo Ayiti"
 lis = koupe_ekzanp(chenn)
 print(lis)
+ # Mete tout premye let chak mo an majiskil:
+chenn="yon chenn ak plizye mo"
+chenn_majiskil= " ".join(word.capitalize()for word in chenn.split() )
+print(chenn_majiskil)
+
+
+#Rekipire premye let ckak mo ak afiche yon nouvo chenn ak tout inisyal sa yo 
+
+chenn ="yon chenn ak plizye mo "
+inisyal_chenn="".join(word[0] for word in chenn.split())
+print(inisyal_chenn)
+
+# Ranplase tout karakte "a" nan yon pa "@"
+
+chenn ="yon chenn ak plizye mo "
+nouvo_chenn =chenn.replace("a","@")
+print(nouvo_chenn)
+
+
+#mete yon chenn karakte devan deye ,epi mete l an majiskil
+chenn ="Ayiti"
+nouvo_chenn= chenn[::-1].capitalize()
+print(nouvo_chenn)
+
+
+
+#Afiche eneks premye karakte "a"nan yon chenn
+chenn ="Ayiti kapab avanse "
+endeks =chenn.find("a")
+print(endeks)
+
+#Afiche total tout endeks karakte "a" nan yon chenn (ki se a majiskil oubyen miniskil)
+chenn ="Ayiti kapab avanse"
+total_endeks=[i for i, letter in enumerate(chenn) if letter == "a" or letter == "A"]
+print(len(total_endeks))
+
+#Kreye yon lis ki gen endeks tout karakte "a" nan yon chenn (selman a miniskil)
+chenn ="Ayiti Kapab Avanse "
+endeks_a_moiniskil =  [i for i, letter in enumerate(chenn) if letter == "a"]
+print(endeks_a_moiniskil)
+
+# Retire tout espas ki nan chenn , epi kole chenn sa ak kantite kakracte li genyen  
+chenn ="yon chenn ak espas "
+chenn_san_espas=chenn.Replace ("","")
+print(chenn_san_espas)
+
+
+
+
+
+
 
